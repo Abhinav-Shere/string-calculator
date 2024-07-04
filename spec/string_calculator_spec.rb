e7b81e79307d40d798ba8bdfbebc32bd0ac09124
abhinav <abhhinavdev12@gmail.com>
2024-07-04 15:45:39 +0530

    added test case for different delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,4 +25,10 @@ RSpec.describe StringCalculator, "#add" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
   end
+
+  context 'when given a different delimiter' do
+    it 'returns the sum of the numbers' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+  end
 end
