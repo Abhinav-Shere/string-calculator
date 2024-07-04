94dfb1c4566b287e318f71df5f803c1bfe313f52
abhinav <abhhinavdev12@gmail.com>
2024-07-04 15:30:02 +0530

    fix second case

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,5 +4,8 @@ class StringCalculator
 	def self.add(numbers)
 		# Return 0 if the input string is empty
     return 0 if numbers.empty?
+
+    # Return number itself if the input string is single value
+    numbers.to_i
 	end
 end
