c51999aec0ba6ab12a41d20baf42e64faec9412c
abhinav <abhhinavdev12@gmail.com>
2024-07-04 15:27:52 +0530

    added test case for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,7 +2,15 @@ require 'rspec'
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator, "#add" do
-  it "returns 0 for empty string" do
-    expect(described_class.add("")).to eql(0)
+  context 'when given an empty string' do
+    it "returns 0 for empty string" do
+      expect(described_class.add("")).to eql(0)
+    end
+  end
+
+  context 'when given a single number' do
+    it 'returns the number' do
+      expect(StringCalculator.add('1')).to eq(1)
+    end
   end
 end
