f991cb60ae401d38b7468f5f05cb2371295e4842
abhinav <abhhinavdev12@gmail.com>
2024-07-04 15:44:19 +0530

    added test for numbers with new line

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -19,4 +19,10 @@ RSpec.describe StringCalculator, "#add" do
       expect(StringCalculator.add('1,5')).to eq(6)
     end
   end
+
+  context 'when given numbers separated by new lines' do
+    it 'returns the sum of the numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+  end
 end
